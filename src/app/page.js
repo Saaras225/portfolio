da3d0ec5ea0775ff48d23e3deccd259635353e10
Saaras225 <sarayance@gmail.com>
2023-10-13 14:17:54 -0300

    fixing navbar button display

--- src/app/page.js
+++ src/app/page.js
@@ -84,20 +84,7 @@ export default function Home() {
                   onClick={() => setNavbar(!navbar)}
                 >
                   {navbar ? (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-6 h-6 text-white"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  ) : (
-                    <svg
+                      <svg
                       xmlns="http://www.w3.org/2000/svg"
                       className="w-6 h-6 text-white"
                       fill="none"
@@ -111,7 +98,22 @@ export default function Home() {
                         d="M4 6h16M4 12h16M4 18h16"
                       />
                     </svg>
-                  )}
+                  ) :
+                    (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-6 h-6 text-white"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    )
+                   }
                 </button>
           </div>
           <div id='menu' className={menu}>
